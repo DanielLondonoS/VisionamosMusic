e69f0e4adb7f58262e3ed48b4a304e56e8dd4caf
dlondono <dlondono@visionamos.com>
2020-02-24 13:05:37 -0500

    Se crea estructura del proyecto con los modelos

---
+++ VisionamosMusic/Data/DataModels/Album.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VisionamosMusic.Data.DataModels
+{
+    /// <summary>
+    /// Desarrollador: Daniel Londoño
+    /// Fecha: 24/02/2020
+    /// Descripcion: Entidad que se encarga de almacenar los albunes musicales
+    /// </summary>
+    public class Album
+    {
+        /// <summary>
+        /// Identificador del album
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Nombre del album
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Fecha de pulicacion del album
+        /// </summary>
+        public DateTime PublishDate { get; set; }
+    }
+}
